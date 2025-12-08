36d8272e5e7ba9edd9900073f3aa6632e0368246
oliullahakib <oliullahakib@gmail.com>
2025-12-08 14:35:51 +0600

    added top decorators section

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import TopDecorators from '../TopDecorators/TopDecorators';
 
 const Home = () => {
     return (
         <div>
            <Banner/>
+           <TopDecorators/>
         </div>
     );
 };
---
+++ src/pages/Shared/SectionTitle.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SectionTitle = ({title,className}) => {
+    return <h1 className={`${className} text-6xl text-accent-content garamond-font`}>{title}</h1>
+};
+
+export default SectionTitle;
