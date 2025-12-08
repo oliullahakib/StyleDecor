793282476897c29925efae4d5a20f2ddac11fef1
oliullahakib <oliullahakib@gmail.com>
2025-12-08 16:28:37 +0600

    added services section to home page

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import TopDecorators from '../TopDecorators/TopDecorators';
+import Services from '../Services/Services';
 
 const Home = () => {
     return (
         <div>
            <Banner/>
+           <Services/>
            <TopDecorators/>
         </div>
     );
