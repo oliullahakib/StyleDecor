a974e83afc0810301ff9225de09a200b95248161
oliullahakib <oliullahakib@gmail.com>
2025-12-09 15:53:29 +0600

    register page added

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { Link, useLocation, useNavigate } from 'react-router';
+import toast from 'react-hot-toast';
+import useAuth from '../../hook/useAuth';
+
+const Login = () => {
+    const {loginUser}=useAuth()
+    const { register, handleSubmit, formState: { errors } } = useForm()
+    const location = useLocation()
+    const navigate = useNavigate()
+    const handleLogin = (data) => {
+        loginUser(data.email,data.password)
+        .then(()=>{
+            toast.success("Login Successfully")
+            navigate(location?.state ||'/')
+        })
+    }
+    return (
+        <div className=' w-full'>
+            <div className="card-body w-full justify-center items-center">
+                <form onSubmit={handleSubmit(handleLogin)} className="fieldset md:w-96">
+                    <label className="label">Email</label>
+                    <input {...register("email", { required: 'Email is required.' })} type="email" className="input w-full" placeholder="Email" />
+                    {errors.email && <p className='text-red-500'>{errors.email.message}</p>}
+                    <label className="label">Password</label>
+                    <input {...register("password", {
+                        required: 'Password is required.',
+                        minLength:
+                        {
+                            value: 6,
+                            message: 'Password must be at least 6 characters long.'
+                        },
+                        pattern: {
+                            value: /^(?=.*[A-Z]).+$/,
+                            message: 'Password must contain An Uppercase letters.',
+                        },
+                        validate: (value) => {
+                            const lowerCaseReg = /(?=.*[a-z])/
+                            if (!lowerCaseReg.test(value)) {
+                                return 'Password must include at least one lowercase letter.';
+                            }
+                            return true
+                        }
+
+                    })} type="password" className="input w-full" placeholder="Password" />
+                    {errors.password &&
+                        <p className='text-red-500'>
+                            {errors.password.message}
+                        </p>}
+                    <div><a className="link link-hover">Forgot password?</a></div>
+                    <button className="btn btn-primary text-black mt-4">Login</button>
+                        <p className='font-semibold'>New to ZapShift? Please <Link state={location?.state} to={"/register"} className='text-green-400 underline'>Register</Link></p>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router";
 import RootLayout from "../layouts/RootLayout";
 import Home from "../pages/Home/Home/Home";
 import ServiceCoveragePage from "../pages/ServiceCoverage/ServiceCoveragePage";
+import Login from "../pages/Login/Login";
+import Register from "../pages/Register/Register";
 
 
 export const router = createBrowserRouter([
@@ -18,6 +20,14 @@ export const router = createBrowserRouter([
                 Component:ServiceCoveragePage,
                 loader:()=>fetch('/serviceCenters.json')
             },
+            {
+                path:"login",
+                Component:Login
+            },
+            {
+                path:"register",
+                Component:Register
+            }
         ]
     }
 ])
