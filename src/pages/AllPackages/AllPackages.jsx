63fe804e21ac21534f3e83ecffda0264f2476f75
oliullahakib <oliullahakib@gmail.com>
2025-12-10 21:43:43 +0600

    added sort by type functionality to all packages page

--- src/pages/AllPackages/AllPackages.jsx
+++ src/pages/AllPackages/AllPackages.jsx
@@ -7,19 +7,20 @@ import PackageCard from '../Shared/PackageCard';
 
 
 const AllPackages = () => {
-    const [search, setSearch] = useState()
+    const [search, setSearch] = useState('')
+    const [type, setType] = useState('')
+    const [filterHidden, setFilterHidden] = useState(true)
     const axiosSecure = useAxiosSecure()
     const { data: packages = [] } = useQuery({
-        queryKey: ['packages',search],
+        queryKey: ['packages', search,type],
         queryFn: async () => {
-            const res = await axiosSecure.get(`/packages?search=${search}`)
+            const res = await axiosSecure.get(`/packages?search=${search}&type=${type}`)
             return res.data
         }
     })
-
     return (
         <MyDiv >
-            <div className='flex items-center justify-between' >
+            <div className='flex flex-col md:flex-row items-center justify-between' >
                 <h2 className='text-xl font-semibold my-8 pl-5'>All Packages <span className='text-primary font-bold'>({packages.length})</span>
                 </h2>
                 {/* search */}
@@ -27,10 +28,35 @@ const AllPackages = () => {
                     <div className="join">
                         <div>
                             <label className="input validator join-item">
-                                <input onChange={(e)=>setSearch(e.target.value)} type="text" placeholder="Search here" />
+                                <input onChange={(e) => setSearch(e.target.value)} type="text" className='md:min-w-80' placeholder="Search here" />
                             </label>
                         </div>
                     </div>
+                </div>
+                {/* sort section  */}
+                <div>
+                    <button onClick={() => setFilterHidden(!filterHidden)} className={`btn ${!filterHidden && "hidden"}`}>Filter</button>
+                    {
+                        !filterHidden && <>
+                            <div>
+                                {/* type sort */}
+                                <div>
+                                    <select onChange={(e)=>setType(e.target.value)} defaultValue="Pick a type" className="select">
+                                        <option disabled={true}>Pick a type</option>
+                                        <option>Home</option>
+                                        <option>Wedding</option>
+                                        <option>Office</option>
+                                        <option>Seminar</option>
+                                        <option>Meeting</option>
+                                    </select>
+                                </div>
+                                {/* price */}
+                                <div>
+
+                                </div>
+                            </div>
+                        </>
+                    }
                 </div>
             </div>
             <div className='grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4 px-3 mb-8'>
