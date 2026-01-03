7193117122cde9ec1586864af670940b38e5ec81
oliullahakib <oliullahakib@gmail.com>
2026-01-03 22:00:41 +0600

    added short description to package card

--- src/pages/Shared/PackageCard.jsx
+++ src/pages/Shared/PackageCard.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router';
 
 const PackageCard = ({ service }) => {
+    console.log(service)
     return (
         <div className="card bg-base-100 shadow-sm relative">
             <figure>
@@ -13,6 +14,7 @@ const PackageCard = ({ service }) => {
             <div className="px-2 py-5 flex-1 flex flex-col justify-end">
                 <h2 className="text-sm md:text-lg">{service.service_name}</h2>
                 <p className='text-sm mt-3 md:text-lg' >Category: <span className='text-accent'>{service.service_category}</span> </p>
+                <p className='text-sm mt-3 md:text-lg' >Description: <span className='text-accent'>{service.description.length > 30 ? service.description.slice(0, 30) + '...' : service.description}</span> </p>
                 <div className=" flex-1 flex flex-col justify-end">
                     <p className='mt-5 text-sm md:text-xl'>Cost: <span className='text-accent-content font-bold'>{service.cost}tk</span> /<span className='text-[8px] lg:text-sm tex-accent'>{service.unit}</span> </p>
                    <Link to={`/package/${service._id}`} className='btn btn-primary mt-3'>View Details</Link>
