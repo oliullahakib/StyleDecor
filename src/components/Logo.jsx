bcb415b57f58d55f401c06cc0b765a0aa3219bde
oliullahakib <oliullahakib@gmail.com>
2026-01-04 16:52:22 +0600

    added logo to dashboard

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router';
 
 const Logo = () => {
-    return <Link> <h1 className='text-2xl text-accent-content font-semibold garamond-font'>Style<span className='text-secondary font-bold'>Decor</span></h1></Link>
+    return <Link to={'/'}> <h1 className='text-2xl text-accent-content font-semibold garamond-font'>Style<span className='text-secondary font-bold'>Decor</span></h1></Link>
 };
 
 export default Logo;
--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -64,12 +64,19 @@ const DashboardLayout = () => {
                 <div className="flex min-h-full flex-col items-start bg-base-200 is-drawer-close:w-14 is-drawer-open:w-64">
                     {/* Sidebar content here */}
                     <ul className="menu w-full grow">
+                    
                         {/* List item */}
                         <li>
                             <Link to={'/'} className="is-drawer-close:tooltip is-drawer-close:tooltip-right" data-tip="Homepage">
+                                <h1 className="is-drawer-close:hidden text-primary text-2xl">Style <span className='text-secondary'>StyleDecor</span></h1>
+                            </Link>
+                        </li>
+                        {/* List item */}
+                        <li>
+                            <Link to={'/dashboard'} className="is-drawer-close:tooltip is-drawer-close:tooltip-right" data-tip="Homepage">
                                 {/* Home icon */}
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor" className="my-1.5 inline-block size-4"><path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8"></path><path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path></svg>
-                                <span className="is-drawer-close:hidden">Home\</span>
+                                <span className="is-drawer-close:hidden">Home</span>
                             </Link>
                         </li>
                         {/*my profile List item */}
